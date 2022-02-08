23c812f8672a2600472660d472009775e3e91c64
Michael Anderson <drmikeando@gmail.com>
2022-02-08 10:07:54 +0800

    Added Bounds::{expand_by,contains}

--- src/nbase/bounds.rs
+++ src/nbase/bounds.rs
@@ -5,3 +5,23 @@ pub struct Bounds<const N: usize> {
     pub min: Point<N>,
     pub max: Point<N>,
 }
+
+impl<const N: usize> Bounds<N> {
+    pub fn expand_by(&self, d: f32) -> Bounds<N> {
+        let unit = Point::from([1.0; N]);
+        Bounds {
+            min: self.min - unit * d,
+            max: self.max + unit * d,
+        }
+    }
+
+    pub fn contains(&self, p: Point<N>) -> bool {
+        if (p - self.min).min() < 0.0 {
+            return false;
+        }
+        if (self.max - p).min() < 0.0 {
+            return false;
+        }
+        return true;
+    }
+}
