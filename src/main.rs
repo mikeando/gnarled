0ed75b40c0dcff94763a3bc0bd232f945958e4ea
Michael Anderson <drmikeando@gmail.com>
2022-03-07 19:58:40 +0800

    Add sierpinski example

--- src/main.rs
+++ src/main.rs
@@ -11,6 +11,7 @@ use n2::lineset::LineSet;
 use n2::polyline::PolyLine;
 use n2::traits::Rotatable;
 use n3::Consumer;
+use nbase::point::Point;
 use nbase::polyline::LineSegment;
 use nbase::traits::*;
 
@@ -35,14 +36,15 @@ fn main() -> Result<(), std::io::Error> {
     // test_3d()?;
     // test_3d_sphere()?;
 
-    tokio::runtime::Builder::new_multi_thread()
-        .enable_all()
-        .build()
-        .unwrap()
-        .block_on(async {
-            test_shader_combined().await
-        })?;
+    test_sierpinski()?;
 
+    // tokio::runtime::Builder::new_multi_thread()
+    //     .enable_all()
+    //     .build()
+    //     .unwrap()
+    //     .block_on(async {
+    //         test_shader_combined().await
+    //     })?;
 
     Ok(())
 }
@@ -511,6 +513,105 @@ pub fn test_3d_sphere() -> Result<(), std::io::Error> {
         },
     )
     .unwrap();
+
+    writeln!(f, "</svg>")?;
+
+    Ok(())
+}
+
+pub struct Triangle {
+    vertices: [Point<2>; 3],
+}
+
+impl Triangle {
+    pub fn scale(self, s: f32) -> Triangle {
+        let mp = (self.vertices[0] + self.vertices[1] + self.vertices[2]) * (1.0 / 3.0);
+        Triangle {
+            vertices: self.vertices.map(|t| Point::lerp(s, mp, t)),
+        }
+    }
+    pub fn rotate(self, th: f32) -> Triangle {
+        let mp = (self.vertices[0] + self.vertices[1] + self.vertices[2]) * (1.0 / 3.0);
+        let vertices = self.vertices.map(|x| x - mp).map(|x| {
+            p2(
+                x.vs[0] * f32::cos(th) - x.vs[1] * f32::sin(th),
+                x.vs[0] * f32::sin(th) + x.vs[1] * f32::cos(th),
+            ) + mp
+        });
+        Triangle { vertices }
+    }
+    pub fn sierpinski_refine(self) -> [Triangle; 3] {
+        // let our initial triangle be ABC
+        let A = self.vertices[0];
+        let B = self.vertices[1];
+        let C = self.vertices[2];
+
+        let AB = Point::lerp(0.5, A, B);
+        let BC = Point::lerp(0.5, B, C);
+        let CA = Point::lerp(0.5, C, A);
+
+        return [
+            Triangle {
+                vertices: [A, AB, CA],
+            }
+            .scale(0.95)
+            .rotate(0.1),
+            Triangle {
+                vertices: [B, AB, BC],
+            }
+            .scale(0.95)
+            .rotate(0.1),
+            Triangle {
+                vertices: [C, CA, BC],
+            }
+            .scale(0.95)
+            .rotate(0.1),
+        ];
+    }
+}
+
+pub fn to_polyline(t: Triangle) -> crate::nbase::polyline::PolyLine<2> {
+    PolyLine {
+        ps: vec![t.vertices[0], t.vertices[1], t.vertices[2], t.vertices[0]],
+    }
+}
+
+pub fn to_lines(ts: Vec<Triangle>) -> crate::nbase::lineset::LineSet<2> {
+    let mut ls = crate::nbase::lineset::LineSet { lines: vec![] };
+    for t in ts {
+        ls.lines.push(to_polyline(t))
+    }
+    ls
+}
+
+pub fn test_sierpinski() -> Result<(), std::io::Error> {
+    use std::io::Write;
+
+    let file_name = "test_sierpinski.svg";
+    let mut f = std::fs::File::create(file_name).unwrap();
+    writeln!(
+        f,
+        r#"<svg viewBox="0 0 800 800" xmlns="http://www.w3.org/2000/svg">"#
+    )?;
+
+    let c = p2(400., 400.);
+    let r = 350f32;
+
+    let theta = 2.0 * std::f32::consts::PI / 3.0;
+    let t = Triangle {
+        vertices: [
+            c + p2(f32::cos(0.0), f32::sin(0.0)) * r,
+            c + p2(f32::cos(theta), f32::sin(theta)) * r,
+            c + p2(f32::cos(-theta), f32::sin(-theta)) * r,
+        ],
+    };
+
+    let mut ts = vec![t];
+    for _ in 0..7 {
+        ts = ts.into_iter().flat_map(|t| t.sierpinski_refine()).collect();
+    }
+
+    to_lines(ts).to_svg(&mut f)?;
 
     writeln!(f, "</svg>")?;
 
